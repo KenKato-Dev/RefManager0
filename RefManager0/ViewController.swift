f831e3e44d4feff65a1decb9d58c6f9485b820d4
[katoken] <[ken0113_dev@icloud.com]>
2022-01-19 21:16:32 +0900

    Initial Commit

---
+++ RefManager0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RefManager0
+//
+//  Created by 加藤研太郎 on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
